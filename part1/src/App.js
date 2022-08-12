1882600a036824589563e3ef1067141115541e33
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-08-12 19:14:28 +0300

    part 1 ex 1.3

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,17 +6,18 @@ const Header = (props) => (
 
 const Part = (props) => (
   <>
-    <p>{props.part} {props.exercises}</p>
+    <p>{props.name} {props.exercises}</p>
   </>
 )
 
-const Content = (props) => (
-  <>
-    <Part part={props.p1} exercises={props.e1} />
-    <Part part={props.p2} exercises={props.e2} />
-    <Part part={props.p3} exercises={props.e3} />
-  </>
-)
+const Content = (props) => {
+  return (
+    <>
+      {props.parts
+        .map(part => <Part name={part.name} exercises={part.exercises} key={part.name} />)}
+    </>
+  )
+}
 
 const Total = (props) => (
   <>
@@ -27,18 +28,28 @@ const Total = (props) => (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  const parts = [part1, part2, part3];
+  let total = 0;
+  parts.map(part => part.exercises).forEach(t => total += t);
 
   return (
     <div>
       <Header course={course} />
-      <Content p1={part1} e1={exercises1} p2={part2} e2={exercises2} p3={part3} e3={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content parts={parts} />
+      <Total total={total} />
     </div>
   )
 }
