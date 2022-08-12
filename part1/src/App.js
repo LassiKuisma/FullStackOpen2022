7736076b1a928a1f79298db9eadcdf2abf5a41b4
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-08-12 19:21:49 +0300

    part 1 ex 1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -19,38 +19,42 @@ const Content = (props) => {
   )
 }
 
-const Total = (props) => (
-  <>
-    <p>Number of exercises {props.total}</p>
-  </>
-)
+const Total = (props) => {
+  let total = 0;
+  props.parts.map(part => part.exercises).forEach(t => total += t);
+  return (
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  )
+}
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  let total = 0;
-  parts.map(part => part.exercises).forEach(t => total += t);
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total total={total} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
