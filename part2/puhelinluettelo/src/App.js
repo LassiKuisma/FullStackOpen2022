4a0c7262708810638b6b5bf182ff30ad4fca5faf
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-08-17 22:22:34 +0300

    part 2 ex 2.6

---
+++ part2/puhelinluettelo/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+const Contacts = ({ contacts }) => {
+  return (
+    <div>
+      {contacts.map(contact =>
+        <p key={contact.name}>{contact.name}</p>
+      )}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addContact = (event) => {
+    event.preventDefault();
+
+    let personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject));
+    setNewName('');
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addContact}>
+        <div>
+          name: <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Contacts contacts={persons} />
+    </div>
+  )
+}
+
+export default App
