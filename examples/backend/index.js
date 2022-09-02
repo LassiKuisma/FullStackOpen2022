d5a7dbdd8825cd3b0d1c95b877dfa892ed1440ff
Lassi Kuisma <lassi.kuisma1@gmail.com>
2022-09-02 16:07:23 +0300

    create fly app

--- examples/backend/index.js
+++ examples/backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+app.use(cors())
+
 app.use(express.json())
 
 const requestLogger = (request, response, next) => {
@@ -97,6 +100,6 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on ${PORT}`);
